b331e4c79adb4127fc11d16f883c121135ac6d5b
ahmadi18 <ahmadian.pouya@gmail.com>
2020-11-02 08:38:14 -0500

    Completed the flower area class

--- Assets/Hummingbird/Scripts/FlowerArea.cs
+++ Assets/Hummingbird/Scripts/FlowerArea.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 /// <summary>
@@ -11,7 +12,7 @@ public class FlowerArea : MonoBehaviour
     public const float AreaDiameter = 20f;
 
     //The list of all flower plants in this flower area (flower plants have multiple flowers)
-    private List<GameObject> flowerplants;
+    private List<GameObject> flowerPlants;
 
     // A lookup dictionary for looking up a flower from a nectar collider
     private Dictionary<Collider, Flower> nectarFlowerDictionary;
@@ -21,4 +22,97 @@ public class FlowerArea : MonoBehaviour
     /// </summary>
     public List<Flower> Flowers { get; private set; }
    
+    /// <summary>
+    /// Reset the flowers and flower plants
+    /// </summary>
+    public void ResetFlowers()
+    {
+        //Rotate each flower plant around the Y axis and subtly around X and Z
+        foreach (GameObject flowerPlant in flowerPlants)
+        {
+            float xRotation = UnityEngine.Random.Range(-5f, 5f);
+            float yRotation = UnityEngine.Random.Range(-180f, 180f);
+            float zRotation = UnityEngine.Random.Range(-5f, 5f);
+            flowerPlant.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
+        }
+
+        //Reset each flower
+        foreach (Flower flower in Flowers)
+        {
+            flower.ResetFlower();
+        }
+    }
+
+    /// <summary>
+    /// Gets the <see cref="Flower"/> that a nectar collider belongs to
+    /// </summary>
+    /// <param name="collider"></param> the nectar collider
+    /// <returns></returns>
+    public Flower GetFlowerFromNectar(Collider collider)
+    {
+        return nectarFlowerDictionary[collider];
+    }
+
+    /// <summary>
+    /// Called when the area wakes up
+    /// </summary>
+    private void Awake()
+    {
+        //initialize variables
+        flowerPlants = new List<GameObject>();
+        nectarFlowerDictionary = new Dictionary<Collider, Flower>();
+        Flowers = new List<Flower>();
+    }
+
+    /// <summary>
+    /// Called when the game starts
+    /// </summary>
+    private void Start()
+    {
+        //Find all flowers that are children of this GameObject/Transform
+        FindChildFlowers(transform);
+    }
+
+    /// <summary>
+    /// Recursively finds all flowers and flower plants that are children of a aprent transform
+    /// </summary>
+    /// <param name="parent">The parent of the children to check</param>
+    private void FindChildFlowers(Transform parent)
+    {
+        for (int i=0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+
+            if(child.CompareTag("flower_plant"))
+            {
+                // Found a flower plant, add it to the flower plants list
+                flowerPlants.Add(child.gameObject);
+
+                //Look for flowers within the flower plant
+                FindChildFlowers(child);
+            }
+            else
+            {
+                //Not a flower plant, look for a Flower component
+                Flower flower = child.GetComponent<Flower>();
+                if(flower != null)
+                {
+                    //Found a flower, add it to the Flowers list
+                    Flowers.Add(flower);
+
+                    //Add the nectar collider to the lookup dictionary
+                    nectarFlowerDictionary.Add(flower.nectarCollider, flower);
+
+                    //Note: there are no flowers that are children of other flowers
+                }
+
+                else
+                {
+                    //Flower component not found, so check children
+                    FindChildFlowers(child);
+                }
+            }
+        }
+    }
+    
 }
